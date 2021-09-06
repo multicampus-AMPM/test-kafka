a46e1693907d2f8729b7506416e5668aba7f5a52
inyestar <yuinye@gmail.com>
2021-09-06 21:27:40 +0900

    troubleshooting to 'a bytes-like object is required, not int'

--- modules/collector/producer.py
+++ modules/collector/producer.py
@@ -34,14 +34,15 @@ if __name__ == '__main__':
     topic = "smart-raw"
     date = '2013-04-10'
     serial_numbers = ['MJ0351YNG9Z0XA','MJ0351YNG9WJSA','MJ0351YNG9Z7LA','MJ0351YNGAD37A','MJ0351YNGABYAA','MJ1311YNG7ESHA','S2F0BE6T','W1F0LRXG','6XW099YJ']
-    capacity_bytes = [3000592982016,3000592982016,3000592982016,3000592982016,3000592982016,3000592982016,1500301910016,3000592982016,1500301910016]
+    capacity_bytes = [30005,300059,300059,30005,300059,300059,15003,30006,150016]
     failure = 0
     count = 0
     for _ in range(10):
 
         serial_number = choice(serial_numbers)
         capacity_byte = choice(capacity_bytes)
-        producer.produce(topic, date, serial_number, capacity_byte, failure, callback=delivery_callback)
+        # TODO: check the form of the data
+        producer.produce(topic, capacity_byte, serial_number, callback=delivery_callback)
         count += 1
 
     # Block until the messages are sent.
